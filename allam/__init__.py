1addf574655e8799a34246dff8f2e6d7c09c95a4
SergeyMtt <sb68595@gmail.com>
2024-07-21 08:00:42 +0200

    add files

---
+++ allam/__init__.py
@@ -0,0 +1,10 @@
+# allam/__init__.py
+
+# Define a package-level variable
+__version__ = '1.0.0'
+__date__ = '20.07.2024'
+__all__ = ['allam', 'combustion', 'recuperator']
+
+from .allam import *
+from .combustion import *
+from .recuperator import *
